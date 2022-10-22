ac7bdf44da707648e6de72a774ad7415df605af1
AdiFischer <adifischer1@gmail.com>
2022-10-22 15:59:35 -0400

    added coments

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import express from "express"//always
-import people from './data.js'
-const PORT = 3033
-const app = express()//always
-app.use(express.json())//without this line, line 13 wont work
+import express from "express"//always calling library from npm i express
+import people from './data.js'// import data from data.js file(instead db)
+const PORT = 3033 // declare port 3000-4000 local on our computer
+const app = express()//always Calls the express function "express()" 
+//and puts new Express application inside the app variable 
+//(to start a new Express application)
+app.use(express.json())//without this line, line 14 wont work. Read req in json
 
-app.get('/people', (req, res) => {
+app.get('/people', (req, res) => {// routing methods for the express app
     res.send(people)
 })
 
